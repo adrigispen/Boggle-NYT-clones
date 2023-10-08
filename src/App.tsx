224dc3005a2088efa27afe2dc8a28b9ffc017a41
adrigispen <adrienne.gispen@optilyz.com>
2023-10-08 16:18:14 +0200

    store words found by player, show words found on the board, add function to apply settings & start a game

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,87 @@
-import "./App.css";
 import Settings from "./Components/Settings";
+import { SettingsModal } from "./Components/SettingsModal";
 import { Grid } from "./Components/Grid";
 import { useState } from "react";
 import { getNewGrid, findWord } from "./helpers";
+import { PlayerData } from "./Components/Types";
 
 function App() {
+  // settings state
+  const [showSettings, setShowSettings] = useState(false);
+
   const [settingsData, setSettingsData] = useState({
-    players: ["", ""],
+    players: ["Player 1", "Player 2"],
     size: 4,
     language: "English",
     generousMode: true,
     speedMode: false,
   });
 
+  // grid and selection state
   const initialGrid: string[][] = getNewGrid(
     settingsData.size,
     settingsData.language
   );
 
+  const noHighlights: boolean[][] = Array(settingsData.size)
+    .fill(false)
+    .map(() => Array(settingsData.size).fill(false));
+
+  const [selectionGrid, setSelectionGrid] = useState(noHighlights);
+
   const [grid, setGrid] = useState(initialGrid);
-  const [playerData, setPlayerData] = useState({
-    currentSearch: "",
-    wordsFound: [""],
-  });
+
+  // player data state
+
+  const initialPlayerData: PlayerData[] = Array(settingsData.players.length)
+    .fill("")
+    .map(
+      (p, i) =>
+        ({
+          playerName: settingsData.players[i],
+          currentSearch: "",
+          wordsFound: [""],
+        } as PlayerData)
+    );
+
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [playerData, setPlayerData] = useState(initialPlayerData);
+
+  // look for and display words
+  function setCurrentSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const newPlayerData = playerData.map((data, i) =>
+      i == currentPlayer ? { ...data, currentSearch: e.target.value } : data
+    );
+    setPlayerData(newPlayerData);
+  }
 
   function handleSearch() {
-    const path = findWord(playerData.currentSearch, grid);
-    if (path.length) {
-      const newWords = playerData.wordsFound.concat(playerData.currentSearch);
-      setPlayerData({ wordsFound: newWords, currentSearch: "" });
+    const pathSelectionGrid = findWord(
+      playerData[currentPlayer].currentSearch,
+      grid,
+      settingsData.generousMode
+    );
+    const newWords = pathSelectionGrid.length
+      ? playerData[currentPlayer].wordsFound.concat(
+          playerData[currentPlayer].currentSearch
+        )
+      : playerData[currentPlayer].wordsFound;
+    const newPlayerData = playerData.map((data, i) =>
+      i == currentPlayer
+        ? { ...data, wordsFound: newWords, currentSearch: "" }
+        : data
+    );
+    setPlayerData(newPlayerData);
+    if (pathSelectionGrid.length) {
+      setSelectionGrid(pathSelectionGrid);
     } else {
       // display "word not on the board!"
-      setPlayerData({ ...playerData, currentSearch: "" });
     }
+    setCurrentPlayer(
+      settingsData.speedMode
+        ? (currentPlayer + 1) % settingsData.players.length
+        : currentPlayer
+    );
   }
 
   function handleEnter(e: React.KeyboardEvent<HTMLInputElement>) {
@@ -41,37 +90,47 @@ function App() {
     }
   }
 
+  // create new game
+  function handleGameStart() {
+    setGrid(getNewGrid(settingsData.size, settingsData.language));
+    setSelectionGrid(noHighlights);
+    setPlayerData(initialPlayerData);
+    setShowSettings(false);
+  }
+
   return (
     <>
       <div>
         <h1>Speedy Boggle</h1>
-        <Settings
-          settingsData={settingsData}
-          setSettingsData={setSettingsData}
+        <button onClick={() => setShowSettings(true)}>Settings</button>
+        <SettingsModal isOpen={showSettings}>
+          <Settings
+            settingsData={settingsData}
+            setSettingsData={setSettingsData}
+          />
+          <button onClick={handleGameStart}>Play Game</button>
+        </SettingsModal>
+        <Grid
+          grid={grid}
+          setGrid={setGrid}
+          selectionGrid={selectionGrid}
+          setSelectionGrid={setSelectionGrid}
         />
-        <button
-          onClick={() =>
-            setGrid(getNewGrid(settingsData.size, settingsData.language))
-          }
-        >
-          Play Game
-        </button>
-        <Grid grid={grid} setGrid={setGrid} />
         <label>
           Search for words
           <input
             name="search"
-            value={playerData.currentSearch}
-            onChange={(e) =>
-              setPlayerData({ ...playerData, currentSearch: e.target.value })
-            }
+            value={playerData[currentPlayer].currentSearch}
+            onChange={(e) => {
+              setCurrentSearch(e);
+            }}
             onKeyDown={(e) => handleEnter(e)}
           />
           <button onClick={handleSearch}>🔎</button>
         </label>
         <div>
           <ul>
-            {playerData.wordsFound.map((word, i) => (
+            {playerData[currentPlayer].wordsFound.map((word, i) => (
               <li key={i}>{word}</li>
             ))}
           </ul>
