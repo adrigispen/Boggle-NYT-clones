26978ab5c849c0444c53e1dcb994184ab6013532
adrigispen <adrienne.gispen@optilyz.com>
2023-10-08 16:15:04 +0200

    Rename types file

---
+++ src/Components/Types.ts
@@ -0,0 +1,44 @@
+export interface GridProps {
+  grid: string[][];
+  setGrid: (grid: string[][]) => void;
+  selectionGrid: boolean[][];
+  setSelectionGrid: (grid: boolean[][]) => void;
+}
+
+export interface LetterSquare {
+  row: number;
+  col: number;
+  letter: string;
+}
+
+export interface LetterSquareProps extends LetterSquare {
+  selected: boolean | undefined;
+}
+
+export interface SettingsProps {
+  settingsData: {
+    players: string[];
+    size: number;
+    language: string;
+    generousMode: boolean;
+    speedMode: boolean;
+  };
+  setSettingsData: (settingsData: {
+    players: string[];
+    size: number;
+    language: string;
+    generousMode: boolean;
+    speedMode: boolean;
+  }) => void;
+}
+
+export interface PlayerData {
+  playerName: string;
+  currentSearch: string;
+  wordsFound: string[];
+}
+
+export interface SettingsModalProps {
+  isOpen: boolean;
+  children: JSX.Element[];
+}
